228297954ec6e3db38569d54e8db8cd77048352d
ashish singh <ashusingh15082001@gmail.com>
2021-09-09 09:03:27 +0530

    version 1.0

--- pages/meetUpDetails/[meetUpId].js
+++ pages/meetUpDetails/[meetUpId].js
@@ -36,7 +36,7 @@ export const getStaticPaths = async function () {
 
   connection.close();
   return {
-    fallback: true,
+    fallback: 'blocking',
     paths: dataArray.map((item) => {
       return {
         params: {
